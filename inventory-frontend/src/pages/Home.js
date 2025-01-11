e6796fe4fc2207d242b09c110ae8e960e10a16b3
pratik26198 <pratik260198@gmail.com>
2025-01-11 17:20:25 +0530

    New project

---
+++ inventory-frontend/src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ItemList from '../components/ItemList';
+
+const Home = () => (
+    <div className="container">
+        <ItemList />
+    </div>
+);
+
+export default Home;
